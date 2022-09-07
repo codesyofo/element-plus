8362b4cc000905577e5f2a652f3b50452247659d
codesyofo <codesyofo0709@gmail.com>
2022-09-08 00:21:12 +0800

    fix(components): [tabs] fix tab-change can't change model-value

--- packages/components/tabs/src/tabs.tsx
+++ packages/components/tabs/src/tabs.tsx
@@ -101,8 +101,6 @@ export default defineComponent({
 
     const changeCurrentName = (value: TabPanelName) => {
       currentName.value = value
-      emit(UPDATE_MODEL_EVENT, value)
-      emit('tabChange', value)
     }
 
     const setCurrentName = async (value?: TabPanelName) => {
@@ -128,7 +126,10 @@ export default defineComponent({
       event: Event
     ) => {
       if (tab.props.disabled) return
-      setCurrentName(tabName)
+      if (tabName !== currentName.value) {
+        emit(UPDATE_MODEL_EVENT, tabName)
+        emit('tabChange', tabName)
+      }
       emit('tabClick', tab, event)
     }
 
